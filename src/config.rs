05783d905f6612e53b675820d5b32ec2a30c5a04
Cristian Kubis <cristian.kubis@tsunix.de>
2014-11-15 15:17:41 +0100

    adjusted border color and added kill command

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 use layout;
-use keycode::{MOD_4, Keystroke};
+use keycode::*;
 use std::rc::Rc;
 use std::default::Default;
 use workspaces::WorkspaceConfig;
@@ -15,7 +15,8 @@ pub struct Config {
   pub terminal: String,
   pub terminal_shortcut: Keystroke,
   pub launcher: String,
-  pub launcher_shortcut: Keystroke
+  pub launcher_shortcut: Keystroke,
+  pub kill_shortcut: Keystroke
 }
 
 impl Default for Config {
@@ -23,13 +24,14 @@ impl Default for Config {
     Config {
       workspaces: Vec::from_fn(9, |idx| WorkspaceConfig{tag: (idx + 1).to_string(), screen: 0, layout: layout::to_box(layout::TallLayout::new(1, 0.5, 0.01))}),
       mod_key: MOD_4,
-      border_width: 1,
-      border_color: 0x000000ff,
-      border_focus_color: 0x00ff0000,
+      border_width: 2,
+      border_color: 0x002e2e2e,
+      border_focus_color: 0x002a82e6,
       terminal: String::from_str("xterm"),
       terminal_shortcut: Keystroke{mods: 0, key: String::from_str("Return")},
       launcher: String::from_str("dmenu_run"),
       launcher_shortcut: Keystroke{mods: 0, key: String::from_str("d")},
+      kill_shortcut: Keystroke{mods: MOD_SHIFT, key: String::from_str("q")}
     }
   }
 }
