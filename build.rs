700cde28d125691a98b14afbcd37ef7cf45e8fe1
Cristian Kubis <cristian.kubis@tsunix.de>
2014-11-15 15:16:56 +0100

    added cargo build script for local config file

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::io;
+use std::io::File;
+use std::io::fs;
+use std::io::fs::PathExtensions;
+
+fn main() {
+    let dst = Path::new(concat!(env!("HOME"), "/.xr3wm/config.rs"));
+    if !dst.exists() {
+      fs::mkdir(&dst.dir_path(), io::USER_RWX);
+      let mut f = File::create(&dst).unwrap();
+      f.write_str(
+"pub fn get_config() -> Config {
+  Default::default()
+}").unwrap();
+    }
+}
